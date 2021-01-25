9f81d88534016c23f0c6973db50a9966eafb8b2f
iaugustine <iaugustine9401@gmail.com>
2021-01-24 21:42:05 -0500

    First commit Message

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 21:34:37 2021
+
+@author: Ryan
+"""
+
+
+import streamlit as st
+import pickle
+
+def main():
+    st.text_input(value ='asd')
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
