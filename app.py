bb2c7fb329efa8bf3ccf6a6b17bc717d719fa2a9
iaugustine <iaugustine9401@gmail.com>
2021-02-04 19:44:08 -0500

    Updated comments and model

--- app.py
+++ app.py
@@ -7,26 +7,28 @@ Created on Sun Jan 24 21:34:37 2021
 
 
 import streamlit as st
-import joblib
-import pandas
-
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
+import pickle
+#import sklearn
+#import pandas
+#from sklearn.feature_extraction.text import CountVectorizer
 
 def main():
     
-    countvect = joblib.load(open('count_vect.pkl', 'rb'))
-    model = joblib.load(open('naive_bayes_model.pkl', 'rb'))
+    #countvect = joblib.load(open('count_vect.pkl', 'rb'))
+    countvect  = pickle.load(open('count_vect.pkl', 'rb'))
+    model = pickle.load(open('naive_bayes_model.pkl', 'rb'))
     st.title('Spam Classifier')
     text = st.text_area('Enter verification text')
     if st.button('Predict'):
+        
+        text = [text]
         text = countvect.transform(text)
         result = model.predict(text)
-        result= str(result)
-        st.write('The text is : ', result)
-    
+        #st.write(result)
+        if result == [0]:
+            st.write('This message is NOT SPAM')
+        else:
+            st.write('This message is SPAM')
     
     
 if __name__ == '__main__':
